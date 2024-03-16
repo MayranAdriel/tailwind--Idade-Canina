d33145d011a3a799ce434bf7e0910161c8d03d13
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-03-15 23:53:14 -0300

    ajustando texto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let elemento = document.querySelector("span");
 
 function calculaIdade(idade) {
-  elemento.textContent = `${idade * 7} anos`;
+  elemento.textContent = `${idade * 7} anos humanos`;
 }
